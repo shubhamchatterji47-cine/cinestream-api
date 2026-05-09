93769cf1dedb509c20696467ae24221657c40902
Shubham <shubhamchatterji47.com>
2026-05-09 19:22:32 +0530

    fix: parse postgresql:// URL correctly for Npgsql

--- Program.cs
+++ Program.cs
@@ -154,10 +154,23 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// ✅ FIXED: Using PostgreSQL instead of SQLite
+// ✅ FIXED: Convert postgresql:// URL to Npgsql connection string format
 var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+string connectionString;
+
+if (databaseUrl != null && databaseUrl.StartsWith("postgresql://"))
+{
+  var uri = new Uri(databaseUrl);
+  var userInfo = uri.UserInfo.Split(':');
+  connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+}
+else
+{
+  connectionString = databaseUrl ?? "Host=localhost;Database=cinestream;Username=postgres;Password=postgres";
+}
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(databaseUrl));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddAuthentication(options =>
 {
@@ -252,7 +265,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-// Auto-create DB tables on startup
 using (var scope = app.Services.CreateScope())
 {
   var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
